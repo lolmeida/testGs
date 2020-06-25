1df8e246912ae550275fd7b9ed53e906d76d4d89
lolmeida <39884938+lolmeida@users.noreply.github.com>
2020-06-25 19:49:12 +0100

    Add files via upload

--- my.js
+++ my.js
@@ -1,4 +1,4 @@
-function testGS () {
+function getGS () {
 
 const url = 'https://script.google.com/macros/s/AKfycbxOE1YkaP1O-mbx0thJtk4OBVnD_XHU8Q94rOsr7_z4yTKo5pd9/exec'
     fetch(url)
@@ -8,4 +8,27 @@ const url = 'https://script.google.com/macros/s/AKfycbxOE1YkaP1O-mbx0thJtk4OBVnD
         });
 }
 
-document.getElementById('btn').addEventListener('click',testGS);
+document.getElementById('btn').addEventListener('click',getGS);
+
+
+function addGS () {
+    const url = 'https://script.google.com/macros/s/AKfycbxOE1YkaP1O-mbx0thJtk4OBVnD_XHU8Q94rOsr7_z4yTKo5pd9/exec'
+    let data = {name:'Olivia'};   
+        fetch(url, {
+            method: 'POST', // *GET, PUT, DELETE, etc.
+            mode: 'no-cors', // no-cors, *cors, same-origin
+            cache:'no-cache', // *default, no-cache, reload, force-cache,only-if-cached
+            credentials:'omit', // includ, *same-origin , omit
+            headers: {
+                'Content-Type':'application/json'
+                //'Content-Type':'application/x-www-form-urlencoded'
+            },
+            redirect:'follow', //manual, *follow, error
+            //referrerPolicy: 'no-referrer', // no-referrer, *no-referre-when-downgrade, orig
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+           
+     
+    }
+    
+    document.getElementById('btn2').addEventListener('click',addGS);
