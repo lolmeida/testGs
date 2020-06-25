31b7721c3d8b6b89dee0577c3a613e3ba0e3a1d1
lolmeida <39884938+lolmeida@users.noreply.github.com>
2020-06-25 17:44:19 +0000

    Update my.js

--- my.js
+++ my.js
@@ -1,6 +1,6 @@
 function testGS () {
 
-let url = 'https://script.google.com/macros/s/AKfycbxOE1YkaP1O-mbx0thJtk4OBVnD_XHU8Q94rOsr7_z4yTKo5pd9/exec'
+const url = 'https://script.google.com/macros/s/AKfycbxOE1YkaP1O-mbx0thJtk4OBVnD_XHU8Q94rOsr7_z4yTKo5pd9/exec'
     fetch(url)
         .then(d=> d.json())
         .then( d=>{
